b2433a9a2eb3de17a712cf0b3f97efcc87d50188
Justin Venezuela <jven@mit.edu>
2011-11-03 01:10:49 -0400

    Initial commit of amphoros web application. Currently prints 'Hello world!'. Also committing a .gitignore file.

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+Package for amphoros.
+"""
+
+__author__ = (
+    'Michael Mekonnen (mikemeko@mit.edu),'
+    'Justin Venezuela (jven@mit.edu),'
+    'Paul Woods (pwoods@mit.edu)'
+)
---
+++ routes.py
@@ -0,0 +1,20 @@
+"""
+Request router for amphoros.
+"""
+
+__author__ = (
+    'Michael Mekonnen (mikemeko@mit.edu),'
+    'Justin Venezuela (jven@mit.edu),'
+    'Paul Woods (pwoods@mit.edu)'
+)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+  return 'Hello world!'
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=6170)
