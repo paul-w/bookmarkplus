1f758343389296b86779d4b0cf10d0956d6559f7
Justin Venezuela <jven@mit.edu>
2011-11-24 01:24:48 -0500

    Added jquery. Made example landing.js to show how we should have an js template for each page of our site.

--- src/routes.py
+++ src/routes.py
@@ -17,10 +17,7 @@ from flask import render_template
 from flask import url_for
 from flaskext.mongokit import MongoKit
 
-# Folder containing html template files
-TEMPLATE_FOLDER = 'templates/html'
-
-app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
+app = Flask(__name__)
 app.config['MONGODB_DATABASE'] = MONGODB_DATABASE
 app.config['MONGODB_HOST'] = MONGODB_HOST
 app.config['MONGODB_PORT'] = MONGODB_PORT
@@ -28,7 +25,12 @@ db = Database(app)
 
 @app.route('/', methods=['GET'])
 def home():
-  return render_template('landing.html')
+  return render_template('html/landing.html')
+
+@app.route('/landing_js.js', methods=['GET'])
+def landing_js():
+  return render_template('js/landing.js',
+      message='This is the landing page!')
 
 # TODO(jven): This is just an example of using database.py.
 @app.route('/register/<name>/<email>', methods=['GET'])
---
+++ src/templates/js/landing.js
@@ -0,0 +1,12 @@
+/**
+ * Authors:
+ * Michael Mekonnen (mikemeko@mit.edu)
+ * Paul Woods (pwoods@mit.edu)
+ * Justin Venezuela (jven@mit.edu)
+ * JavaScript for landing page.
+ */
+
+$(document).ready(function() {
+  // TODO(jven): take me out of here!
+  alert('{{ message }}');
+});
