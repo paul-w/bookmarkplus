1f7545e7de465ce73956bdccd2faa9b35241a03e
Michael Mekonnen <mike22meko@gmail.com>
2011-12-05 02:31:40 -0500

    added method in util.js for toggling textarea/holder, in case we want to use it for more than the inputs on the landing page

--- src/templates/js/landing.js
+++ src/templates/js/landing.js
@@ -67,31 +67,9 @@ $(document).ready(function() {
   //   a palce_holder is visible only when the correspodning text_input
   //   is not in focus and is empty
   text_containers = $("div.text_container");
-  for (var i = 0; i < text_containers.length; i++) {
-    var container_ = text_containers[i];
-    var holder_ = $(container_).find("span");
-    var input_ = $(container_).find("input");
-    // If a text_container div is clicked, focus the correspoding text input
-    $(container_).click(function (input) {
-      return function() {
-        input.focus();
-      };
-    }($(input_)));
-    // If a text input is in focus, hide holder
-    $(input_).focusin(function (holder) {
-      return function () {
-        holder.hide();
-      }
-    }($(holder_)));
-    // If a text input is empty and out of focus, show holder
-    $(input_).focusout(function (input, holder) {
-      return function () {
-        if (input.val() === "") {
-          holder.show();
-        }
-      }
-    }($(input_), $(holder_)));
-  }
+  $.each(text_containers, function (idx, container) {
+    UTILS.toggleInputMessage(container);
+  });
 
   // Hide the about div and bind the links.
   $('#about').hide();
