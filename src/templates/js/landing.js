23780c9c5998d254fc6603fdc9b05ce60841648c
Justin Venezuela <jven@mit.edu>
2011-12-08 21:21:25 -0500

    Styled landing.js.

--- src/templates/js/landing.js
+++ src/templates/js/landing.js
@@ -1,73 +1,71 @@
 /**
+ * JavaScript for landing page.
  * Authors:
  * Michael Mekonnen (mikemeko@mit.edu)
  * Paul Woods (pwoods@mit.edu)
  * Justin Venezuela (jven@mit.edu)
- * JavaScript for landing page.
  */
 
 $(document).ready(function() {
 
   // show all flash messages
   {% for message in get_flashed_messages() %}
-    UTILS.showMessage("{{ message }}");
+    UTILS.showMessage('{{ message }}');
   {% endfor %}
 
   // When login form is submitted, make an ajax call to '/login'.
   // On success, redirect to main page.
   // On failure, report the error.
-  var login_form = $("form#login_form");
-  login_form.submit(function () {
-    var email = $("input#login_email").val();
-    var password = $("input#login_password").val();
-    $.post("login", {
-             "email": email,
-             "password": password
-           },
-           function (response) {
-             if (response.type === "error") {
-               UTILS.showMessage(response.error);
-             } else if (response.type === "redirect") {
-               window.location.replace(response.url);
-             } else {
-               // We should never get here
-             }
-           });
+  var login_form = $('form#login_form');
+  login_form.submit(function() {
+    var email = $('input#login_email').val();
+    var password = $('input#login_password').val();
+    $.post('login', {
+       'email': email,
+       'password': password
+     }, function(response) {
+       if (response.type === 'error') {
+         UTILS.showMessage(response.error);
+       } else if (response.type === 'redirect') {
+         window.location.replace(response.url);
+       } else {
+         // We should never get here
+       }
+     });
     return false;
   });
 
   // When register form is submitted, make an ajax call to '/register'.
   // On success, redirect to main page.
   // On failure, report the error.
-  var register_form = $("form#register_form");
-  register_form.submit(function () {
-    var name = $("input#register_name").val();
-    var email = $("input#register_email").val();
-    var password = $("input#register_password").val();
-    var repassword = $("input#register_repassword").val();
-    $.post("register", {
-             "name": name,
-             "email": email,
-             "password": password,
-             "repassword": repassword
-           },
-           function (response) {
-             if (response.type === "error") {
-               UTILS.showMessage(response.error);
-             } else if (response.type === "redirect") {
-               window.location.replace(response.url);
-             } else {
-               // We should never get here
-             }
-           });
+  var register_form = $('form#register_form');
+  register_form.submit(function() {
+    var name = $('input#register_name').val();
+    var email = $('input#register_email').val();
+    var password = $('input#register_password').val();
+    var repassword = $('input#register_repassword').val();
+    $.post('register', {
+        'name': name,
+        'email': email,
+        'password': password,
+        'repassword': repassword
+    }, function(response) {
+      if (response.type === 'error') {
+        UTILS.showMessage(response.error);
+      } else if (response.type === 'redirect') {
+        window.location.replace(response.url);
+      } else {
+        // We should never get here
+      }
+    });
     return false;
   });
 
   // For each place_holder-text_input pair, attach listeners so that
-  //   a palce_holder is visible only when the correspodning text_input
-  //   is not in focus and is empty
-  text_containers = $("div.text_container");
-  $.each(text_containers, function (idx, container) {
+  // a palce_holder is visible only when the correspodning text_input
+  // is not in focus and is empty
+  text_containers = $('div.text_container');
+  $.each(text_containers, function(idx, container) {
     UTILS.toggleInputMessage(container);
   });
 
@@ -81,5 +79,4 @@ $(document).ready(function() {
     $('#about').slideUp();
     $('#register_login').slideDown();
   });
-
 });
