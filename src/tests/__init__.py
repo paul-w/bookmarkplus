78b897937b9dd1862b1e405d013a9cf5b65723c4
Michael Mekonnen <mike22meko@gmail.com>
2011-12-11 01:48:18 -0500

    setup for test suite

---
+++ src/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Test suite.
+"""
+
+__author__ = (
+    'Michael Mekonnen (mikemeko@mit.edu),'
+    'Justin Venezuela (jven@mit.edu),'
+    'Paul Woods (pwoods@mit.edu)'
+)
---
+++ src/tests/python/__init__.py
@@ -0,0 +1,9 @@
+"""
+Python unittests.
+"""
+
+__author__ = (
+    'Michael Mekonnen (mikemeko@mit.edu),'
+    'Justin Venezuela (jven@mit.edu),'
+    'Paul Woods (pwoods@mit.edu)'
+)
